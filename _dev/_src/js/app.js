c8cdda9a115379482405e6a963b9f934798f0c68
隈元 彰平 <kumamoto@kumamoto.local>
2016-09-28 17:58:49 +0900

    メニューページ 鳥コメント以外実装

--- _dev/_src/js/app.js
+++ _dev/_src/js/app.js
@@ -49,22 +49,6 @@
 		    });
 		}
 
-		$('#main_slider').slick({
-			centerMode: true,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			arrows: true,
-			fade: false,
-			dots: true
-		});
-
-		$('#thumb_slider').slick({
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			arrows: true,
-			fade: false
-		});
-
 		// NavShow();
 		SPNav();
 		RollOver();
