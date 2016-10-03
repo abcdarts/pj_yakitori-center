a8f7c0095b78afc1b4ce3c971a269a74d05449f3
隈元 彰平@東京DES <kumamoto-shohei@dmm.com>
2016-10-03 21:03:30 +0900

    店舗検索ページ実装

---
+++ _dev/_src/js/search.js
@@ -0,0 +1,58 @@
+(function(){
+
+	$(function(){
+
+        var w = $(window).width;
+
+        if(w > 1200){
+
+            var sc = $(window).scrollTop();
+
+            if(sc < 320){
+                $('.ttl').addClass('is-active');
+            }
+
+            if(sc < 800 && sc > 600){
+                $('.item-03').animate({
+                    'top': '1140px',
+                    'left': '212px'
+                }, 1000);
+            }
+
+            if(sc > 2450){
+                $('.item-08').addClass('is-active');
+            }
+
+            $(window).on('scroll resize', function(){
+                sc = $(window).scrollTop();
+                console.log(sc);
+
+                if(sc < 320){
+                    $('.ttl').addClass('is-active');
+                }
+
+                if(sc < 800 && sc > 600){
+                    $('.item-03').animate({
+                        'top': '1140px',
+                        'left': '212px'
+                    }, 1000);
+                }
+
+                if(sc > 1200 && sc < 1800){
+                    $('.item-06').animate({
+                        'top': '1876px',
+                        'right': '335px'
+                    }, 5000);
+                }
+
+                if(sc > 2450){
+                    $('.item-08').addClass('is-active');
+                }
+
+            });
+        }
+
+
+
+  });
+})();
